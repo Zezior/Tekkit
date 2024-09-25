df6678ae7d930efa9d8992db951f06e9249d1daa
Zezior <90460321+Zezior@users.noreply.github.com>
2024-09-25 10:38:36 +0100

    1

--- Tekkit SMP/Reactor Mainframe/Reactors/ids.lua
+++ Tekkit SMP/Reactor Mainframe/Reactors/ids.lua
@@ -1,8 +1,9 @@
 -- Reactor ids.lua
 return {
     reactorIDs = {
-        4708,
-        4710
+        4708, -- Temp base
+        4710, -- Temp base
+        4845 -- Reactor 1
     },
     activityCheckID = 4641,     -- Activity Check Computer ID
     powerMainframeID = 4644,    -- Power Mainframe Computer ID
