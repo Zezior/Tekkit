294af3dc5dbc59534ed430000d408532e1cc263e
Zezior <90460321+Zezior@users.noreply.github.com>
2024-09-30 15:11:58 +0100

    1

--- Tekkit SMP/Power Mainframe/ids.lua
+++ Tekkit SMP/Power Mainframe/ids.lua
@@ -4,7 +4,7 @@ return {
     reactorMainframeID = 4709,  -- Replace with your reactor mainframe ID if different
     allowedSenderIDs = {
         4912,   -- Power Sender 1
-        4870,   -- Power AIP 1
+        4969,   -- Xander 10k EU/T
         4709    -- Reactor Mainframe
     }
 }
