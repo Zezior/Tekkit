36e9bc3a8375bbb053ff0dadab78a8dce316ee70
Zezior <90460321+Zezior@users.noreply.github.com>
2024-10-09 22:59:19 +0100

    1

--- Tekkit SMP/Power Mainframe/ids.lua
+++ Tekkit SMP/Power Mainframe/ids.lua
@@ -6,6 +6,7 @@ return {
         4912,   -- Power Sender 1
         4969,   -- Power 1 Xander 30k EU/T
         5006,   -- Power 2 Xander 10k EU/T
-        4709    -- Reactor Mainframe
+        4709,   -- Reactor Mainframe
+        5059    -- GravityCube 1k EU/T
     }
 }
