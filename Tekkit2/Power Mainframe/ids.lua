c30c4405585c8f50380cb3279694df6ead4431a2
Zezior <90460321+Zezior@users.noreply.github.com>
2025-02-10 03:09:43 +0000

    Update ids.lua

--- Tekkit2/Power Mainframe/ids.lua
+++ Tekkit2/Power Mainframe/ids.lua
@@ -3,10 +3,7 @@
 return {
     reactorMainframeID = 4709,  -- Replace with your reactor mainframe ID if different
     allowedSenderIDs = {
-        4912,   -- Power Sender 1
-        4969,   -- Power 1 Xander 30k EU/T
-        5006,   -- Power 2 Xander 10k EU/T
-        4709,   -- Reactor Mainframe
-        5056    -- GravityCube 1k EU/T
+        7566,   -- Power Sender 1
+        7562,   -- Reactor Mainframe
     }
 }
